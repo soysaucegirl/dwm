84fffb624d3f4aff346b9e6fe0e0d7671c0823c8
olive <blackmetalpenguin@gmail.com>
2020-06-10 13:09:14 -0400

    changed icon sizes and workspace names

--- config.h
+++ config.h
@@ -45,7 +45,7 @@ static const unsigned int alphas[][3]      = {
 };
 
 /* tagging */
-static const char *tags[] = { "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX" };
+static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
 
 /* rules */
 static const Rule rules[] = {
